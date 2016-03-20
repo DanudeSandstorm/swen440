847720c5f50785731c06bce5d85c24e56e4f2b00
Steven David Simmons <sds9278@g.rit.edu>
2016-03-20 16:10:38 -0400

    Tutorial Copied
    Finished applying what was on the tutorial to the code and hacking it so
    it mimics getting a bad health check randomly

--- src/main/java/career/fair/app/CareerFairApplication.java
+++ src/main/java/career/fair/app/CareerFairApplication.java
@@ -1,23 +1,54 @@
 package career.fair.app;
 
+import com.codahale.metrics.health.HealthCheck.Result;
 import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
-public class CareerFairApplication extends Application<AppConfiguration> {
+public class CareerFairApplication extends Application<CareerFairConfiguration> {
+	
+	@Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<CareerFairConfiguration> bootstrap) {
+        // nothing to do yet
+    }
 	
 	public static void main(String[] args) throws Exception {
+		System.out.println("Oitati");
+		args = new String[] { "server", "career-fair.yml" };
 		new CareerFairApplication().run(args);
 	}
+	
 	@Override
-	public void run(AppConfiguration configuration, Environment environment) throws Exception {
-		final AppResource resource = new AppResource(
-		        configuration.getCrash()
+	public void run(CareerFairConfiguration configuration, Environment environment) throws Exception {
+		System.out.println("Potato");
+		final CareerFairResource resource = new CareerFairResource(
+				configuration.getTemplate(),
+		        configuration.getDefaultName()
 		    );
 		final TemplateHealthCheck healthCheck =
-		        new TemplateHealthCheck(configuration.getCrash());
-		    environment.healthChecks().register("template", healthCheck);
-		    environment.jersey().register(resource);
+		        new TemplateHealthCheck(configuration.getTemplate());
 		
+		environment.healthChecks().register("template", healthCheck);
+		environment.jersey().register(resource);
+		
+		Result check;
+		
+		// Loops forever, pinging and ponging
+		while(true)
+		{
+			System.out.println("Ping...");
+			
+			check = healthCheck.check();
+			if (!check.isHealthy())
+			{
+				System.out.println("ERROR: Ping failed!!!");
+			}
+		}
 	}
 
 }
--- src/main/java/career/fair/app/TemplateHealthCheck.java
+++ src/main/java/career/fair/app/TemplateHealthCheck.java
@@ -1,21 +1,35 @@
 package career.fair.app;
 
 import com.codahale.metrics.health.HealthCheck;
+import java.util.Random;
 
 public class TemplateHealthCheck extends HealthCheck {
-	
-	private boolean crash;
-	
-	public TemplateHealthCheck(boolean crash){
-		this.crash = crash;
-	}
-	@Override
-	protected Result check() throws Exception {
-		if (crash){
-			return Result.unhealthy("Server is down");
-		} else {
-			return Result.healthy();
-		}
-	}
+    private final String template;
+    private Random rand;
+    
+    public TemplateHealthCheck(String template) {
+        this.template = template;
+        rand = new Random();
+    }
 
+    @Override
+    protected Result check() throws Exception {
+        final String saying = String.format(template, "TEST");
+        
+        int num = rand.nextInt(100);
+        
+        // If the result is even, return unhealthy
+        if (num % 53 == 0)
+        {
+        	return Result.unhealthy("template doesn't include a name");
+        }
+        
+        if (!saying.contains("TEST")) {
+            return Result.unhealthy("template doesn't include a name");
+        }
+        
+        System.out.println("Pong...");
+        
+        return Result.healthy();
+    }
 }
